f0691e8a1a5bdd303c25832841333a4b79b7f18f
Artur Lastovskiy <4372wow@gmail.com>
2023-09-17 12:51:49 +0300

    update AppTest, PlainDiffFormatter

--- app/src/main/java/hexlet/code/Utils/formatters/PlainDiffFormatter.java
+++ app/src/main/java/hexlet/code/Utils/formatters/PlainDiffFormatter.java
@@ -10,66 +10,68 @@ public class PlainDiffFormatter implements DiffFormatter {
     public String formatDiffs(List<DiffAccumulator> diffs) {
         StringBuilder result = new StringBuilder();
 
-        diffs.forEach(i -> {
-            switch (i.getStatus()) {
+        for (int i = 0; i < diffs.size(); i++) {
+            var o = diffs.get(i);
+            switch (o.getStatus()) {
                 case "removed" -> {
                     result.append("Property '");
-                    result.append(i.getKey());
+                    result.append(o.getKey());
                     result.append("' was ");
-                    result.append(i.getStatus());
-                    result.append("\n");
+                    result.append(o.getStatus());
                 }
                 case "added" -> {
                     result.append("Property '");
-                    result.append(i.getKey());
+                    result.append(o.getKey());
                     result.append("' was ");
-                    result.append(i.getStatus());
+                    result.append(o.getStatus());
                     result.append(" with value: ");
-                    if (i.getValue() instanceof List<?> || i.getValue() instanceof Map<?, ?>) {
+                    if (o.getValue() instanceof List<?> || o.getValue() instanceof Map<?, ?>) {
                         result.append("[complex value]");
-                    } else if (i.getValue() instanceof String) {
+                    } else if (o.getValue() instanceof String) {
                         result.append("'");
-                        result.append(i.getValue());
+                        result.append(o.getValue());
                         result.append("'");
                     } else {
-                        result.append(i.getValue());
+                        result.append(o.getValue());
                     }
-                    result.append("\n");
                 }
                 case "updated" -> {
                     result.append("Property '");
-                    result.append(i.getKey());
+                    result.append(o.getKey());
                     result.append("' was ");
-                    result.append(i.getStatus());
+                    result.append(o.getStatus());
                     result.append(".");
                     result.append(" From ");
-                    if (i.getValueBefore() != null && i.getValueBefore() instanceof Collection<?>) {
+                    if (o.getValueBefore() != null && o.getValueBefore() instanceof Collection<?>) {
                         result.append("[complex value]");
-                    } else if (i.getValueBefore() instanceof String) {
+                    } else if (o.getValueBefore() instanceof String) {
                         result.append("'");
-                        result.append(i.getValueBefore());
+                        result.append(o.getValueBefore());
                         result.append("'");
                     } else {
-                        result.append(i.getValueBefore());
+                        result.append(o.getValueBefore());
                     }
                     result.append(" to ");
-                    if (i.getValueAfter() != null && i.getValueAfter() instanceof Collection<?>) {
+                    if (o.getValueAfter() != null && o.getValueAfter() instanceof Collection<?>) {
                         result.append("[complex value]");
-                    } else if (i.getValueAfter() instanceof String) {
+                    } else if (o.getValueAfter() instanceof String) {
                         result.append("'");
-                        result.append(i.getValueAfter());
+                        result.append(o.getValueAfter());
                         result.append("'");
                     } else {
-                        result.append(i.getValueAfter());
+                        result.append(o.getValueAfter());
                     }
-                    result.append("\n");
                 }
                 case "unchanged" -> {
                     // do nothing when status is unchanged
                 }
-                default -> throw new IllegalArgumentException("Unexpected value in switch: " + i.getStatus());
+                default -> throw new IllegalArgumentException("Unexpected value in switch: " + o.getStatus());
             }
-        });
+
+            if (i < diffs.size() - 1 && !o.getStatus().equals("unchanged")) {
+                result.append("\n");
+            }
+        }
 
         return result.toString();
     }
