b16ba20973289ea04c0d9215955a39ecbde983da
Artur Lastovskiy <4372wow@gmail.com>
2023-09-17 12:08:40 +0300

    update AppTest, StylishDiffFormatter

--- app/src/main/java/hexlet/code/Utils/formatters/StylishDiffFormatter.java
+++ app/src/main/java/hexlet/code/Utils/formatters/StylishDiffFormatter.java
@@ -11,33 +11,33 @@ public class StylishDiffFormatter implements DiffFormatter {
         diffs.forEach(i -> {
             switch (i.getStatus()) {
                 case "removed" -> {
-                    result.append(" - ");
+                    result.append("  - ");
                     result.append(i.getKey());
                     result.append(": ");
                     result.append(i.getValue());
                     result.append("\n");
                 }
                 case "added" -> {
-                    result.append(" + ");
+                    result.append("  + ");
                     result.append(i.getKey());
                     result.append(": ");
                     result.append(i.getValue());
                     result.append("\n");
                 }
                 case "updated" -> {
-                    result.append(" - ");
+                    result.append("  - ");
                     result.append(i.getKey());
                     result.append(": ");
                     result.append(i.getValueBefore());
                     result.append("\n");
-                    result.append(" + ");
+                    result.append("  + ");
                     result.append(i.getKey());
                     result.append(": ");
                     result.append(i.getValueAfter());
                     result.append("\n");
                 }
                 case "unchanged" -> {
-                    result.append("   ");
+                    result.append("    ");
                     result.append(i.getKey());
                     result.append(": ");
                     result.append(i.getValue());
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -23,12 +23,12 @@ class AppTest {
         String actual = generate(filePath1, filePath2, format);
         String expected = """
                 {
-                 - follow: false
-                   host: hexlet.io
-                 - proxy: 123.234.53.22
-                 - timeout: 50
-                 + timeout: 20
-                 + verbose: true
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
                 }""";
         assertThat(actual).isEqualTo(expected);
     }
@@ -41,12 +41,12 @@ class AppTest {
         String actual = generate(filePath1, filePath2, format);
         String expected = """
                 {
-                 - follow: false
-                   host: hexlet.io
-                 - proxy: 123.234.53.22
-                 - timeout: 50
-                 + timeout: 20
-                 + verbose: true
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
                 }""";
         assertThat(actual).isEqualTo(expected);
     }
@@ -59,29 +59,29 @@ class AppTest {
         String actual = generate(filePath1, filePath2, format);
         String expected = """
                 {
-                   chars1: [a, b, c]
-                 - chars2: [d, e, f]
-                 + chars2: false
-                 - checked: false
-                 + checked: true
-                 - default: null
-                 + default: [value1, value2]
-                 - id: 45
-                 + id: null
-                 - key1: value1
-                 + key2: value2
-                   numbers1: [1, 2, 3, 4]
-                 - numbers2: [2, 3, 4, 5]
-                 + numbers2: [22, 33, 44, 55]
-                 - numbers3: [3, 4, 5]
-                 + numbers4: [4, 5, 6]
-                 + obj1: {nestedKey=value, isNested=true}
-                 - setting1: Some value
-                 + setting1: Another value
-                 - setting2: 200
-                 + setting2: 300
-                 - setting3: true
-                 + setting3: none
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
         assertThat(actual).isEqualTo(expected);
     }
@@ -94,29 +94,29 @@ class AppTest {
         String actual = generate(filePath1, filePath2, format);
         String expected = """
                 {
-                   chars1: [a, b, c]
-                 - chars2: [d, e, f]
-                 + chars2: false
-                 - checked: false
-                 + checked: true
-                 - default: null
-                 + default: [value1, value2]
-                 - id: 45
-                 + id: null
-                 - key1: value1
-                 + key2: value2
-                   numbers1: [1, 2, 3, 4]
-                 - numbers2: [2, 3, 4, 5]
-                 + numbers2: [22, 33, 44, 55]
-                 - numbers3: [3, 4, 5]
-                 + numbers4: [4, 5, 6]
-                 + obj1: {nestedKey=value, isNested=true}
-                 - setting1: Some value
-                 + setting1: Another value
-                 - setting2: 200
-                 + setting2: 300
-                 - setting3: true
-                 + setting3: none
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
         assertThat(actual).isEqualTo(expected);
     }
