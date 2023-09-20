80050affd565c345c840aea582c7a28703c5a98a
Artur Lastovskiy <4372wow@gmail.com>
2023-09-20 22:01:11 +0300

    update project

--- app/src/main/java/hexlet/code/formatters/JsonDiffFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonDiffFormatter.java
@@ -3,7 +3,7 @@ package hexlet.code.formatters;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.DiffAccumulator;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -17,12 +17,14 @@ public final class JsonDiffFormatter implements DiffFormatter {
             if (o.getStatus().equals("removed") || o.getStatus().equals("added")) {
                 diffsMap.put(o.getKey(), o.getValue());
             } else if (o.getStatus().equals("updated")) {
-                diffsMap.put(o.getKey(), o.getValueAfter());
+                List<Object> objList = new ArrayList<>();
+                objList.add(o.getValueBefore());
+                objList.add(o.getValueAfter());
+                diffsMap.put(o.getKey(), objList);
             } else {
                 diffsMap.put(o.getKey(), o.getValue());
             }
         });
-
 
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         return ow.writeValueAsString(diffsMap);
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -3,6 +3,8 @@ package hexlet.code;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -17,107 +19,12 @@ class AppTest {
     }
 
     @Test
-    public void testGenerateDefaultJson() throws Exception {
-        String filePath1 = absolutePath + "/file1.json";
-        String filePath2 = absolutePath + "/file2.json";
-        String actual = generate(filePath1, filePath2);
-        String expected = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void testGenerateDefaultYaml() throws Exception {
-        String filePath1 = absolutePath + "/file1.yml";
-        String filePath2 = absolutePath + "/file2.yml";
-        String actual = generate(filePath1, filePath2);
-        String expected = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void testGenerateJsonStylish() throws Exception {
-        String filePath1 = absolutePath + "/file1.json";
-        String filePath2 = absolutePath + "/file2.json";
-        String format = "stylish";
-        String actual = generate(filePath1, filePath2, format);
-        String expected = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void testGenerateYamlStylish() throws Exception {
-        String filePath1 = absolutePath + "/file1.yml";
-        String filePath2 = absolutePath + "/file2.yml";
-        String format = "stylish";
-        String actual = generate(filePath1, filePath2, format);
-        String expected = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
     public void testGenerateJsonNestedStylish() throws Exception {
         String filePath1 = absolutePath + "/jsonNested1.json";
         String filePath2 = absolutePath + "/jsonNested2.json";
-        String format = "stylish";
-        String actual = generate(filePath1, filePath2, format);
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey=value, isNested=true}
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
+        String stylishTest = absolutePath + "/stylishTest.txt";
+        String actual = generate(filePath1, filePath2);
+        String expected = new String(Files.readAllBytes(Paths.get(stylishTest)));
         assertThat(actual).isEqualTo(expected);
     }
 
@@ -125,34 +32,9 @@ class AppTest {
     public void testGenerateYamlNestedStylish() throws Exception {
         String filePath1 = absolutePath + "/yamlNested1.yml";
         String filePath2 = absolutePath + "/yamlNested2.yml";
-        String format = "stylish";
-        String actual = generate(filePath1, filePath2, format);
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey=value, isNested=true}
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
+        String stylishTest = absolutePath + "/stylishTest.txt";
+        String actual = generate(filePath1, filePath2);
+        String expected = new String(Files.readAllBytes(Paths.get(stylishTest)));
         assertThat(actual).isEqualTo(expected);
     }
 
@@ -160,22 +42,21 @@ class AppTest {
     public void testGenerateJsonNestedPlain() throws Exception {
         String filePath1 = absolutePath + "/jsonNested1.json";
         String filePath2 = absolutePath + "/jsonNested2.json";
+        String plainTest = absolutePath + "/plainTest.txt";
         String format = "plain";
         String actual = generate(filePath1, filePath2, format);
-        String expected = """
-                Property 'chars2' was updated. From [complex value] to false
-                Property 'checked' was updated. From false to true
-                Property 'default' was updated. From null to [complex value]
-                Property 'id' was updated. From 45 to null
-                Property 'key1' was removed
-                Property 'key2' was added with value: 'value2'
-                Property 'numbers2' was updated. From [complex value] to [complex value]
-                Property 'numbers3' was removed
-                Property 'numbers4' was added with value: [complex value]
-                Property 'obj1' was added with value: [complex value]
-                Property 'setting1' was updated. From 'Some value' to 'Another value'
-                Property 'setting2' was updated. From 200 to 300
-                Property 'setting3' was updated. From true to 'none'""";
+        String expected = new String(Files.readAllBytes(Paths.get(plainTest)));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGenerateYamlNestedPlain() throws Exception {
+        String filePath1 = absolutePath + "/yamlNested1.yml";
+        String filePath2 = absolutePath + "/yamlNested2.yml";
+        String plainTest = absolutePath + "/plainTest.txt";
+        String format = "plain";
+        String actual = generate(filePath1, filePath2, format);
+        String expected = new String(Files.readAllBytes(Paths.get(plainTest)));
         assertThat(actual).isEqualTo(expected);
     }
 
@@ -190,7 +71,7 @@ class AppTest {
                   "follow" : false,
                   "host" : "hexlet.io",
                   "proxy" : "123.234.53.22",
-                  "timeout" : 20,
+                  "timeout" : [ 50, 20 ],
                   "verbose" : true
                 }""";
         assertThat(actual).isEqualTo(expected);
